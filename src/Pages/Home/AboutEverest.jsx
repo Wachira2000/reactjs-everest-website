d29a374b423fc5b9e4adff1f062357a4d935082d
Wachira2000 <lewiswachira996@gmail.com>
2024-03-03 17:56:25 -0800

    Changes

--- src/Pages/Home/AboutEverest.jsx
+++ src/Pages/Home/AboutEverest.jsx
@@ -10,10 +10,7 @@ export default function AboutMe() {
           <h1 className="skills-section--heading">About Everest</h1>
           <p className="hero--section-description">
           At Everest, we're more than just a team of developers we're your strategic partners in navigating the digital landscape. With a passion for innovation and a commitment to excellence, we specialize in crafting bespoke web and mobile solutions that empower businesses to thrive in today's dynamic market.
-
 Our approach is simple yet powerful: we listen, we understand, and we execute. Whether you're facing complex business challenges or seeking to enhance your online presence, we're here to turn your ideas into reality. With expertise in operations, automation, AI integration, and digital marketing, we offer comprehensive solutions tailored to your unique needs.
-
-
           </p>
           <p className="hero--section-description">
           At Everest, we don't just build websites and apps – we build success stories. Join us on a journey to digital greatness, where your vision becomes our mission, and together, we reach new heights.
